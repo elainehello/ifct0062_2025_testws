287d5d1c25c8ac69d5be646af0546907ed0091b7
elainehello <195331169+elainehello@users.noreply.github.com>
2025-05-15 17:38:09 +0200

    --

---
+++ src/main/java/com/elainevalles/getafe/ifct0062_2025_testws/Repository/IMovie.java
@@ -0,0 +1,5 @@
+package com.elainevalles.getafe.ifct0062_2025_testws.Repository;
+
+
+public interface IMovie {
+}
--- src/main/java/com/elainevalles/getafe/ifct0062_2025_testws/model/Movie.java
+++ src/main/java/com/elainevalles/getafe/ifct0062_2025_testws/model/Movie.java
@@ -1,83 +1,27 @@
 package com.elainevalles.getafe.ifct0062_2025_testws.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class Movie {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
-    private String genre;
-    private String url;
+    private String director;
     private int year;
+    private String poster;
 
-    public Movie() {
-    }
-
-    public Movie(String title, String genre, String url, int year) {
-        this.title = title;
-        this.genre = genre;
-        this.url = url;
-        this.year = year;
-    }
-
-    public Movie(Integer id, String title, String genre, String url, int year) {
-        this.id = id;
-        this.title = title;
-        this.genre = genre;
-        this.url = url;
-        this.year = year;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    @Override
-    public String toString() {
-        return "movie{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", genre='" + genre + '\'' +
-                ", url='" + url + '\'' +
-                ", year=" + year +
-                '}';
-    }
 }
