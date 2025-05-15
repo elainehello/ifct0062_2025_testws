5a5289d9bbbb177d8080705c0809094a35009b17
elainehello <195331169+elainehello@users.noreply.github.com>
2025-05-15 17:05:58 +0200

    javabean

---
+++ src/main/java/com/elainevalles/getafe/ifct0062_2025_testws/model/movie.java
@@ -0,0 +1,81 @@
+package com.elainevalles.getafe.ifct0062_2025_testws.model;
+
+import jakarta.persistence.Id;
+
+public class movie {
+    @Id
+    private Long id;
+    private String title;
+    private String genre;
+    private String url;
+    private int year;
+
+    public movie() {
+    }
+
+    public movie(String title, String genre, String url, int year) {
+        this.title = title;
+        this.genre = genre;
+        this.url = url;
+        this.year = year;
+    }
+
+    public movie(Long id, String title, String genre, String url, int year) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.url = url;
+        this.year = year;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", url='" + url + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
